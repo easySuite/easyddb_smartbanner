e72f7f8c567a334ac9c7c3e70467eceff5cc003c
Nicolae Grigoriu <ng@inlead.dk>
2017-03-27 09:59:55 +0300

    EASYOPAC-421 - Image manipulations on banner.

--- js/easyddb_smartbanner.js
+++ js/easyddb_smartbanner.js
@@ -1,27 +1,34 @@
 /**
- * Created by Max on 15.03.2017.
+ * Initiate SmartBanner object.
  */
 
 (function ($) {
-  $(document).ready(function () {
-    console.log(Drupal.settings.easyddb_smartbanner);
-    new SmartBanner({
-      daysHidden: Drupal.settings.easyddb_smartbanner.days_hidden,   // days to hide banner after close button is clicked (defaults to 15)
-      daysReminder: Drupal.settings.easyddb_smartbanner.days_reminder, // days to hide banner after "VIEW" button is clicked (defaults to 90)
-      appStoreLanguage: Drupal.settings.easyddb_smartbanner.app_store_language, // language code for the App Store (defaults to user's browser language)
-      title: Drupal.settings.easyddb_smartbanner.app_title,
-      author: Drupal.settings.easyddb_smartbanner.app_author,
-      button: Drupal.settings.easyddb_smartbanner.button_name,
-      store: {
-        ios: Drupal.settings.easyddb_smartbanner.apple.description,
-        android: Drupal.settings.easyddb_smartbanner.android.description,
-        windows: Drupal.settings.easyddb_smartbanner.windows.description
-      },
-      price: {
-        ios: Drupal.settings.easyddb_smartbanner.apple.price,
-        android: Drupal.settings.easyddb_smartbanner.android.price,
-        windows: Drupal.settings.easyddb_smartbanner.windows.price
-      }
-    });
-  });
+  Drupal.behaviors.easyddb_smartbanner = {
+    attach: function(context, settings) {
+      var easyddb_smartbanner = settings.easyddb_smartbanner;
+      var store = {};
+      var price = {};
+
+      var os = ['android', 'ios', 'windows'];
+      os.forEach(function (item) {
+        if (Drupal.settings.easyddb_smartbanner[item]) {
+          store[item] = easyddb_smartbanner[item].description;
+          price[item] = easyddb_smartbanner[item].price;
+        }
+      });
+
+      new SmartBanner({
+        daysHidden: Drupal.settings.easyddb_smartbanner.days_hidden,   // days to hide banner after close button is clicked (defaults to 15)
+        daysReminder: Drupal.settings.easyddb_smartbanner.days_reminder, // days to hide banner after "VIEW" button is clicked (defaults to 90)
+        appStoreLanguage: Drupal.settings.easyddb_smartbanner.app_store_language, // language code for the App Store (defaults to user's browser language)
+        title: Drupal.settings.easyddb_smartbanner.app_title,
+        author: Drupal.settings.easyddb_smartbanner.app_author,
+        button: Drupal.settings.easyddb_smartbanner.button_name,
+        store: store,
+        price: price,
+        icon: easyddb_smartbanner.app_icon.icon_path
+      });
+    }
+  };
+
 })(jQuery);
