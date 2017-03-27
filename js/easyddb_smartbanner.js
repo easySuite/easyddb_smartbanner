cb607ec8c02937b8d7d8a6b7ae26d07ff332d1d8
Nicolae Grigoriu <ng@inlead.dk>
2017-03-27 21:38:44 +0300

    EASYOPAC-421 - Fix notices.

--- js/easyddb_smartbanner.js
+++ js/easyddb_smartbanner.js
@@ -10,24 +10,27 @@
       var price = {};
 
       var os = ['android', 'ios', 'windows'];
-      os.forEach(function (item) {
-        if (Drupal.settings.easyddb_smartbanner[item]) {
-          store[item] = easyddb_smartbanner[item].description;
-          price[item] = easyddb_smartbanner[item].price;
-        }
-      });
+      if (Drupal.settings.easyddb_smartbanner) {
+        os.forEach(function (item) {
+          if (Drupal.settings.easyddb_smartbanner[item] !== undefined) {
+            store[item] = easyddb_smartbanner[item].description;
+            price[item] = easyddb_smartbanner[item].price;
+          }
+        });
 
-      new SmartBanner({
-        daysHidden: Drupal.settings.easyddb_smartbanner.days_hidden,   // days to hide banner after close button is clicked (defaults to 15)
-        daysReminder: Drupal.settings.easyddb_smartbanner.days_reminder, // days to hide banner after "VIEW" button is clicked (defaults to 90)
-        appStoreLanguage: Drupal.settings.easyddb_smartbanner.app_store_language, // language code for the App Store (defaults to user's browser language)
-        title: Drupal.settings.easyddb_smartbanner.app_title,
-        author: Drupal.settings.easyddb_smartbanner.app_author,
-        button: Drupal.settings.easyddb_smartbanner.button_name,
-        store: store,
-        price: price,
-        icon: easyddb_smartbanner.app_icon.icon_path
-      });
+        new SmartBanner({
+          daysHidden: Drupal.settings.easyddb_smartbanner.days_hidden,   // days to hide banner after close button is clicked (defaults to 15)
+          daysReminder: Drupal.settings.easyddb_smartbanner.days_reminder, // days to hide banner after "VIEW" button is clicked (defaults to 90)
+          appStoreLanguage: Drupal.settings.easyddb_smartbanner.app_store_language, // language code for the App Store (defaults to user's browser language)
+          title: Drupal.settings.easyddb_smartbanner.app_title,
+          author: Drupal.settings.easyddb_smartbanner.app_author,
+          button: Drupal.settings.easyddb_smartbanner.button_name,
+          store: store,
+          price: price,
+          icon: easyddb_smartbanner.app_icon.icon_path
+        });
+      }
+
     }
   };
 
